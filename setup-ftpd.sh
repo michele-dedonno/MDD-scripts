19ff25f6fb86de10896180b9a8718a0cce2a349c
Michele De Donno <michelededonno@gmail.com>
2023-01-17 19:31:07 +0100

    Create setup-ftpd.sh

---
+++ setup-ftpd.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#   Copyright (C) 2023 Michele De Donno
+
+#   % ============================== LICENSE ============================== %
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>
+#   % ====================================================================== %
+
+
+#   % ======================= DESCRIPTION AND USAGE ======================= &
+#    Bash script to setup the ftpd after first installation
+#
+#     Example of usage:
+#		  $ sudo ./setup-ftpd.sh 
+#
+#     This script is inspired from the Offensive Security course "PEN-200"
+#   % ====================================================================== %
+#
+
+# ============= EDIT THIS VALUES BEFORE RUNNING  ============= #
+sgroup=ftpgroup
+suser=kali-ftp
+ftpuser=kaliftpd
+ftphome=/ftphome
+# ============ ============ ============ ============ ======== #
+
+echo "[+] configuring FTPd server using the following data.";
+echo "[>] OS user: $suser"
+echo "[>] OS group: $sgroup"
+echo "[>] FTPd user: $ftpuser"
+echo "[>] FTPd user home: $ftphome"
+
+# create ftpgroup
+sudo groupadd $sgroup
+# create new sytem user
+echo "[*] Enter password for the user '$suser'."
+sudo useradd -g $sgroup -d /dev/null -s /etc $suser
+# configure Pure-FTPd
+sudo pure-pw useradd $ftpuser -u $suser -d $ftphome
+sudo pure-pw mkdb
+cd /etc/pure-ftpd/auth/
+sudo ln -s ../conf/PureDB 60pdb
+sudo mkdir -p $ftphome
+sudo chown -R $suser:$sgroup $ftphome
+sudo systemctl restart pure-ftpd
+
+echo "[+] Finished."
